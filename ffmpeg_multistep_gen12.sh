cfbde578a3dc1b7f8ffd6995232a5d36ac5b0839
Norman Zhi <normanzyb@gmail.com>
2025-05-25 21:37:29 +0100

    Slightly increase H264 bit-rate

--- ffmpeg_multistep_gen12.sh
+++ ffmpeg_multistep_gen12.sh
@@ -54,7 +54,7 @@ $ffmpeg -hide_banner -hwaccel qsv -hwaccel_output_format qsv $input \
         \
         -init_hw_device opencl=ocl -filter_hw_device ocl \
         -filter:v:2 scale_qsv=w=1280:h=720,hwdownload,format=p010le,hwupload,tonemap_opencl=format=nv12:p=bt709:t=bt709:m=bt709:tonemap=hable:peak=300:desat=0,hwdownload,format=nv12 \
-        -c:v:2 h264_qsv -b:v:2 3200k -profile:v:2 baseline $qsv_h264_params \
+        -c:v:2 h264_qsv -b:v:2 3500k -profile:v:2 baseline $qsv_h264_params \
         \
         -map 0:v:0 -map 0:v:1 -map 0:v:1 \
         -map 0:a:0 -map 0:a:0 \
@@ -65,10 +65,10 @@ $ffmpeg -hide_banner -hwaccel qsv -hwaccel_output_format qsv $input \
             \
             -c:v:0 copy -tag:v:0 hvc1 -b:v:0 40M \
             -c:v:1 copy -tag:v:1 hvc1 -b:v:1 10M \
-            -c:v:2 copy -tag:v:2 avc1 -b:v:2 3200k \
+            -c:v:2 copy -tag:v:2 avc1 -b:v:2 3500k \
             \
             -filter:v:3 scale_qsv=format=nv12:w=854:h=480 \
-            -c:v:3 h264_qsv -tag:v:3 avc1 -b:v:3 1500k -profile:v:3 baseline $qsv_h264_params \
+            -c:v:3 h264_qsv -tag:v:3 avc1 -b:v:3 1800k -profile:v:3 baseline $qsv_h264_params \
             \
             -map 0:v:0 -map 0:v:1 -map 0:v:2 -map 0:v:2 \
             -map 0:a:0 -map 0:a:1 \
@@ -117,8 +117,8 @@ $ffmpeg -hide_banner -hwaccel qsv -hwaccel_output_format qsv $input \
                 -c:a:0 copy -b:a:0 160k \
                 -c:a:1 copy -b:a:1 160k \
                 \
-                -c:v:0 copy -tag:v:0 avc1 -b:v:0 3200k \
-                -c:v:1 copy -tag:v:1 avc1 -b:v:1 1500k \
+                -c:v:0 copy -tag:v:0 avc1 -b:v:0 3500k \
+                -c:v:1 copy -tag:v:1 avc1 -b:v:1 1800k \
                 \
                 -map 0:v:2 -map 0:v:3 \
                 -map 0:a:1 -map 0:a:1 \
--- ffmpeg_test_gen12.sh
+++ ffmpeg_test_gen12.sh
@@ -48,7 +48,7 @@ $ffmpeg -hide_banner -hwaccel qsv -hwaccel_output_format qsv $input \
         \
         -init_hw_device opencl=ocl -filter_hw_device ocl \
         -filter:v:2 scale_qsv=w=1280:h=720,hwdownload,format=p010le,hwupload,tonemap_opencl=format=nv12:p=bt709:t=bt709:m=bt709:tonemap=hable:peak=300:desat=0,hwdownload,format=nv12 \
-        -c:v:2 h264_qsv -b:v:2 3200k -profile:v:2 baseline $qsv_h264_params \
+        -c:v:2 h264_qsv -b:v:2 3500k -profile:v:2 baseline $qsv_h264_params \
         \
         -map 0:v:0 -map 0:v:1 -map 0:v:1 \
         -map 0:a:0 -map 0:a:0 \
@@ -59,10 +59,10 @@ $ffmpeg -hide_banner -hwaccel qsv -hwaccel_output_format qsv $input \
             \
             -c:v:0 copy -tag:v:0 hvc1 -b:v:0 40M \
             -c:v:1 copy -tag:v:1 hvc1 -b:v:1 10M \
-            -c:v:2 copy -tag:v:2 avc1 -b:v:2 3200k \
+            -c:v:2 copy -tag:v:2 avc1 -b:v:2 3500k \
             \
             -filter:v:3 scale_qsv=format=nv12:w=854:h=480 \
-            -c:v:3 h264_qsv -tag:v:3 avc1 -b:v:3 1500k -profile:v:3 baseline $qsv_h264_params \
+            -c:v:3 h264_qsv -tag:v:3 avc1 -b:v:3 1800k -profile:v:3 baseline $qsv_h264_params \
             \
             -map 0:v:0 -map 0:v:1 -map 0:v:2 -map 0:v:2 \
             -map 0:a:0 -map 0:a:1 \
@@ -84,21 +84,21 @@ exit
         # -init_hw_device vaapi=va -init_hw_device qsv=qs@va -init_hw_device opencl=ocl@va -filter_hw_device qs \
             # -init_hw_device vaapi=va:/dev/dri/renderD128,driver=iHD -init_hw_device qsv=qs@va -init_hw_device opencl=ocl@va -filter_hw_device ocl \
 
-            # -c:v:3 h264_qsv -b:v:3 3200k -profile:v:3 baseline $qsv_h264_params \
+            # -c:v:3 h264_qsv -b:v:3 3500k -profile:v:3 baseline $qsv_h264_params \
 
 # vpp_qsv=procamp=1:saturation=1.10:contrast=1.01:brightness=5:out_range=limited
 
             # -filter:v:3 hwupload,tonemap_opencl=format=nv12:p=bt709:t=bt709:m=bt709:tonemap=hable:peak=100:desat=0,hwdownload,format=nv12 \
-            # -c:v:3 libx264 -profile:v:3 baseline -crf 25 -maxrate 3200k -bufsize 8M -preset ultrafast -tune zerolatency \
+            # -c:v:3 libx264 -profile:v:3 baseline -crf 25 -maxrate 3500k -bufsize 8M -preset ultrafast -tune zerolatency \
 
             # -filter:v:3 hwmap=derive_device=opencl,tonemap_opencl=format=nv12:p=bt709:t=bt709:m=bt709:tonemap=hable:peak=300:desat=0,hwmap=derive_device=qsv \
-            # -c:v:3 h264_qsv -b:v:3 3200k -profile:v:3 baseline $qsv_h264_params \
+            # -c:v:3 h264_qsv -b:v:3 3500k -profile:v:3 baseline $qsv_h264_params \
 
             # -filter:v:3 vpp_qsv=tonemap=1,vpp_qsv=procamp=1:saturation=1.10:contrast=1.01:brightness=5:out_range=limited,vpp_qsv=format=nv12:out_color_matrix=bt709:out_color_primaries=bt709:out_color_transfer=bt709 \
-            # -c:v:3 h264_qsv -b:v:3 3200k -profile:v:3 baseline $qsv_h264_params \
+            # -c:v:3 h264_qsv -b:v:3 3500k -profile:v:3 baseline $qsv_h264_params \
 
             # -filter:v:3 zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,zscale=t=bt709:m=bt709:r=tv,format=yuv420p \
-            # -c:v:3 libx264 -profile:v:3 baseline -crf 25 -maxrate 3200k -bufsize 8M -preset ultrafast -tune zerolatency \
+            # -c:v:3 libx264 -profile:v:3 baseline -crf 25 -maxrate 3500k -bufsize 8M -preset ultrafast -tune zerolatency \
 
 
 
