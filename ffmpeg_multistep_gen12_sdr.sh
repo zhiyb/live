ae89e8a6c9bccd617bf0bcc7148987cc5495bd35
Norman Zhi <normanzyb@gmail.com>
2025-06-03 22:47:33 +0100

    Update ffmpeg_multistep_gen12*.sh to send rist stream to vps

--- ffmpeg_multistep_gen12_sdr.sh
+++ ffmpeg_multistep_gen12_sdr.sh
@@ -36,6 +36,11 @@ $ffmpeg -hide_banner -hwaccel qsv -hwaccel_output_format qsv $input \
     \
     -c:a:0 copy \
     -c:v:0 copy -b:v:0 40M \
+    -f mpegts "${rec}_src.ts" -y \
+    \
+    \
+    -c:a:0 copy \
+    -c:v:0 copy -b:v:0 40M \
     \
     -filter:v:1 scale_qsv=w=1920:h=1080 \
     -c:v:1 hevc_qsv -b:v:1 10M -profile:v:1 main $qsv_hevc_params \
@@ -44,70 +49,24 @@ $ffmpeg -hide_banner -hwaccel qsv -hwaccel_output_format qsv $input \
     -f mpegts - | $ffmpeg -hide_banner -hwaccel qsv -hwaccel_output_format qsv -f mpegts -i - \
         \
         -c:a:0 copy \
+        -c:v:0 copy \
+        -map 0:v:1 -map 0:a:0 \
+        -f segment -segment_format mpegts -strftime 1 -segment_time 24:00:00 "${rec}_%Y-%m-%dT%H-%M-%S%z.ts" \
+        \
+        \
+        -c:a:0 copy \
         -c:a:1 aac -b:a:1 160k -ac 2 \
         \
         -c:v:0 copy \
         -c:v:1 copy \
         \
-        -filter:v:2 scale_qsv=w=1280:h=720 \
+        -filter:v:2 scale_qsv=format=nv12:w=1280:h=720 \
         -c:v:2 h264_qsv -b:v:2 3500k -profile:v:2 baseline $qsv_h264_params \
         \
-        -map 0:v:0 -map 0:v:1 -map 0:v:1 \
+        -filter:v:3 scale_qsv=format=nv12:w=854:h=480 \
+        -c:v:3 h264_qsv -b:v:3 1800k -profile:v:3 baseline $qsv_h264_params \
+        \
+        -map 0:v:0 -map 0:v:1 -map 0:v:1 -map 0:v:1 \
         -map 0:a:0 -map 0:a:0 \
-        -f mpegts - | $ffmpeg -hide_banner -hwaccel qsv -hwaccel_output_format qsv -f mpegts -i - \
-            \
-            -c:a:0 copy \
-            -c:a:1 copy \
-            \
-            -c:v:0 copy -tag:v:0 hvc1 -b:v:0 40M \
-            -c:v:1 copy -tag:v:1 hvc1 -b:v:1 10M \
-            -c:v:2 copy -tag:v:2 avc1 -b:v:2 3500k \
-            \
-            -filter:v:3 scale_qsv=format=nv12:w=854:h=480 \
-            -c:v:3 h264_qsv -tag:v:3 avc1 -b:v:3 1800k -profile:v:3 baseline $qsv_h264_params \
-            \
-            -map 0:v:0 -map 0:v:1 -map 0:v:2 -map 0:v:2 \
-            -map 0:a:0 -map 0:a:1 \
-            -f mpegts - | $ffmpeg -hide_banner -f mpegts -i - \
-                \
-                -c:a:0 copy \
-                -c:v:0 copy \
-                -map 0:v:1 -map 0:a:0 \
-                -f segment -segment_format mpegts -strftime 1 -segment_time 24:00:00 "${rec}_%Y-%m-%dT%H-%M-%S%z.ts" \
-                \
-                \
-                -c:a:0 copy \
-                -c:v:0 copy \
-                -map 0:v:3 -map 0:a:1 \
-                -f flv rtmp://vps.wg:1935/live/${name} \
-                \
-                \
-                -c:a:0 copy \
-                \
-                -c:v:0 copy -tag:v:0 hvc1 -b:v:0 40M \
-                -c:v:1 copy -tag:v:1 hvc1 -b:v:1 10M \
-                \
-                -map 0:v:0 -map 0:v:1 \
-                -map 0:a:0 \
-                -f hls \
-                -var_stream_map "a:0,name:audio,agroup:audio,default:yes "\
-"v:0,name:src,agroup:audio "\
-"v:1,name:1080_hevc,agroup:audio "\
-                -master_pl_name "${name}-hevc.m3u8" $hls_params \
-                "${dst}-%v.m3u8" \
-                \
-                \
-                -c:a:0 copy -b:a:0 160k \
-                -c:a:1 copy -b:a:1 160k \
-                \
-                -c:v:0 copy -tag:v:0 avc1 -b:v:0 3500k \
-                -c:v:1 copy -tag:v:1 avc1 -b:v:1 1800k \
-                \
-                -map 0:v:2 -map 0:v:3 \
-                -map 0:a:1 -map 0:a:1 \
-                -f hls \
-                -var_stream_map \
-"v:0,a:0,name:720_h264_audio "\
-"v:1,a:1,name:480_h264_audio "\
-                -master_pl_name "${name}.m3u8" $hls_params \
-                "${dst}-%v.m3u8"
+        -f mpegts -overrun_nonfatal 1 -fifo_size 262144 \
+            "rist://vps.wg:19352?cname=live_mvar&session-timeout=5000&keepalive-interval=2000" \
